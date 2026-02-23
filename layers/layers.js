e340b2f70da60921a08194d54d7f0cc16eb64394
Anita Gudelj <149238912+AnitaGudelj@users.noreply.github.com>
2026-02-23 12:27:41 +0100

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,58 @@
+ol.proj.proj4.register(proj4);
+//ol.proj.get("EPSG:3857").setExtent([1760089.480080, 5317498.444548, 1887022.390926, 5449720.226680]);
+var wms_layers = [];
+
+
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap',
+            'type':'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var lyr_SailingPC_Ljeto_2017_2023_mean_1 = new ol.layer.Image({
+        opacity: 1,
+        
+    title: 'SailingPC_Ljeto_2017_2023_mean<br />\
+    <img src="styles/legend/SailingPC_Ljeto_2017_2023_mean_1_0.png" /> 0,0000<br />\
+    <img src="styles/legend/SailingPC_Ljeto_2017_2023_mean_1_1.png" /> 0,1444<br />\
+    <img src="styles/legend/SailingPC_Ljeto_2017_2023_mean_1_2.png" /> 0,2888<br />\
+    <img src="styles/legend/SailingPC_Ljeto_2017_2023_mean_1_3.png" /> 0,4331<br />\
+    <img src="styles/legend/SailingPC_Ljeto_2017_2023_mean_1_4.png" /> 0,5775<br />' ,
+        
+        
+        source: new ol.source.ImageStatic({
+            url: "./layers/SailingPC_Ljeto_2017_2023_mean_1.png",
+            attributions: ' ',
+            projection: 'EPSG:3857',
+            alwaysInRange: true,
+            imageExtent: [-9763441.130289, 1684718.735672, 10868458.806786, 22743541.353028]
+        })
+    });
+var lyr_protection_3857_2 = new ol.layer.Image({
+        opacity: 1,
+        
+    title: 'protection_3857<br />\
+    <img src="styles/legend/protection_3857_2_0.png" /> 1<br />\
+    <img src="styles/legend/protection_3857_2_1.png" /> 1<br />' ,
+        
+        
+        source: new ol.source.ImageStatic({
+            url: "./layers/protection_3857_2.png",
+            attributions: ' ',
+            projection: 'EPSG:3857',
+            alwaysInRange: true,
+            imageExtent: [-9763441.130289, 1684541.353028, 10868558.869711, 22743541.353028]
+        })
+    });
+var group_Natura2000_end2024 = new ol.layer.Group({
+                                layers: [lyr_protection_3857_2,],
+                                fold: 'open',
+                                title: 'Natura2000_end2024'});
+
+lyr_OpenStreetMap_0.setVisible(true);lyr_SailingPC_Ljeto_2017_2023_mean_1.setVisible(true);lyr_protection_3857_2.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_SailingPC_Ljeto_2017_2023_mean_1,group_Natura2000_end2024];
